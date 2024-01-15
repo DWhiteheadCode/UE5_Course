1a8d24c262516a4540b0d7271419166ec1fc2030
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-01-15 14:45:09 +0800

    Create explosive barrel

---
+++ Source/UE5_Course/Private/CExplosiveBarrel.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CExplosiveBarrel.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ACExplosiveBarrel::ACExplosiveBarrel()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("Static Mesh");
+	MeshComp->SetCollisionProfileName("PhysicsActor");
+	MeshComp->SetSimulatePhysics(true);
+
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>("Radial Force");
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->ForceStrength = 2000.0f;
+	RadialForceComp->Radius = 700.0f;
+}
+
+void ACExplosiveBarrel::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	MeshComp->OnComponentHit.AddDynamic(this, &ACExplosiveBarrel::OnHit);
+}
+
+
+
+
+void ACExplosiveBarrel::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+}
+
+
+
+
+// Called when the game starts or when spawned
+void ACExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UE5_Course/Public/CExplosiveBarrel.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CExplosiveBarrel.generated.h"
+
+class UStaticMeshComponent;
+class URadialForceComponent;
+
+UCLASS()
+class UE5_COURSE_API ACExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACExplosiveBarrel();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* RadialForceComp;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void PostInitializeComponents() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
