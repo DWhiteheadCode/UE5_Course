8d8280386e9a34faecd86593386b6f2a8fac0fe5
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-02-13 16:23:17 +0800

    Add AI ranged attack and basic EQS

---
+++ Source/UE5_Course/Private/AI/CBTTask_RangedAttack.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/CBTTask_RangedAttack.h"
+
+#include "AiController.h"
+#include "GameFramework/Character.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UCBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (ensure(MyController))
+	{
+		ACharacter* MyPawn = Cast<ACharacter>(MyController->GetPawn());
+		if (MyPawn == nullptr) // Not sure why this isn't an ensure()
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		AActor* TargetActor = Cast<AActor>( OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if (TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+		
+		FVector MuzzleLocation = MyPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+		FVector DirectionToTarget = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = DirectionToTarget.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProjectile = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, Params);
+
+		// Return Succeeded if the projectile successfully spawn, return Faield otherwise
+		return NewProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+}
