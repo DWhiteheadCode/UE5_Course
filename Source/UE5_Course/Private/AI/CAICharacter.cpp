df258058d57836eb98d93c62d88aa6d21f47561f
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-02-19 13:14:53 +0800

    AI character death

--- Source/UE5_Course/Private/AI/CAICharacter.cpp
+++ Source/UE5_Course/Private/AI/CAICharacter.cpp
@@ -7,6 +7,7 @@
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "CAttributeComponent.h"
+#include "BrainComponent.h"
 
 // Sets default values
 ACAICharacter::ACAICharacter()
@@ -22,6 +23,7 @@ void ACAICharacter::PostInitializeComponents()
     Super::PostInitializeComponents();
 
     PawnSensingComp->OnSeePawn.AddDynamic(this, &ACAICharacter::OnPawnSeen);
+    AttributeComp->OnHealthChanged.AddDynamic(this, &ACAICharacter::OnHealthChanged);
 }
 
 void ACAICharacter::OnPawnSeen(APawn* Pawn)
@@ -32,5 +34,25 @@ void ACAICharacter::OnPawnSeen(APawn* Pawn)
         UBlackboardComponent* BlackboardComp = AIC->GetBlackboardComponent();
         BlackboardComp->SetValueAsObject("TargetActor", Pawn);
         //DrawDebugString(GetWorld(), GetActorLocation(), "PLAYER SPOTTED", nullptr, FColor::White, 4.0f, true);
+    }
+}
+
+void ACAICharacter::OnHealthChanged(AActor* InstigatorActor, UCAttributeComponent* OwningComp, float NewHealth, float Delta)
+{
+    if (Delta < 0.0f && NewHealth <= 0.0f)
+    {
+        // Stop BehaviorTree
+        AAIController* AIC = Cast<AAIController>(GetController());
+        if (AIC)
+        {
+            AIC->GetBrainComponent()->StopLogic("Killed");
+        }
+
+        // Ragdoll
+        GetMesh()->SetAllBodiesSimulatePhysics(true);
+        GetMesh()->SetCollisionProfileName("Ragdoll");
+
+        // Call Destroy() in 5 seconds
+        SetLifeSpan(5.0f);
     }
 }
